018a2f953bf0a802d348d19a23626cc88cef9eb7
ItsOKayCZ <oldrich.krupauer@pm.me>
2023-08-27 22:21:57 +0200

    Increased reduce_temperature variable

--- Python Trainer/variables.py
+++ Python Trainer/variables.py
@@ -3,7 +3,7 @@ max_trained_epochs = 500
 
 # Hyperparameters
 start_temperature = 20
-reduce_temperature = 1/20 # 0.75
+reduce_temperature = 1/15 # 0.75
 num_training_examples = 100
 discount = 0.95 # devalues future reward
 learning_rate = 0.0005
