Wrong shape bug fix (#50)
* Fixing wrong shape in the Unity environment

* Added build to branch

* Pushes the build to branch

* Runs action when changes in unity_env folder

---------

Co-authored-by: ItsOKayCZ <ItsOKayCZ@users.noreply.github.com>
Co-authored-by: Tadeáš Báča <bacatade@fit.cvut.cz>
Co-authored-by: tad34s <119794635+tad34s@users.noreply.github.com>

* Change dimension of image -> 160x120, add interactive flag

* Changed texture dimensions correctly

* Refactoring train.py

* Setting shape to real life


---------

Co-authored-by: Tadeáš Báča <bacatade@fit.cvut.cz>
Co-authored-by: ItsOKayCZ <ItsOKayCZ@users.noreply.github.com>
Co-authored-by: tad34s <119794635+tad34s@users.noreply.github.com>
Co-authored-by: tad34s <tad34s@users.noreply.github.com>

python_trainer/train.py
@@ -1,8 +1,7 @@
 import argparse
 import datetime
 import json
-import os
-import threading
+from pathlib import Path
 
 import torch
 from data_channel import DataChannel
@@ -10,6 +9,7 @@ from keyboard_listener import KeyboardListener
 from mlagents_envs.environment import UnityEnvironment
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
 from network import QNetwork
+from tensorboard import program
 
 # for TensorBoard
 from torch.utils.tensorboard import SummaryWriter
@@ -25,82 +25,85 @@ from variables import (
     VISUAL_INPUT_SHAPE,
 )
 
-writer = SummaryWriter()
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--num-areas", type=int, default=1)
 parser.add_argument("-s", "--save-model", action="store_true")
 parser.add_argument(
     "-e",
     "--env",
-    default="./Python Trainer/builds/distance_reward_old_env/selfDriving.x86_64",
+    default=str(Path(__file__).parent / "build" / "StandaloneLinux64" / "selfDriving"),
 )
 parser.add_argument("-D", "--no-display", action="store_true")
 parser.add_argument("-t", "--time-scale", type=float, default=1.0)
+parser.add_argument("-i", "--interactive", action="store_true")
 args = parser.parse_args()
 NUM_AREAS = args.num_areas
 SAVE_MODEL = args.save_model
 ENV_PATH = args.env
 NO_DISPLAY = args.no_display
 TIME_SCALE = args.time_scale
-
-engine_channel = EngineConfigurationChannel()
-data_channel = DataChannel()
-
-listener = KeyboardListener()
-listener.start()
+INTERACTIVE = args.interactive
 
 
-def launch_tensor_board():
-    import os
+def launch_tensor_board(logs_location: Path) -> None:
+    tb = program.TensorBoard()
+    tb.configure(argv=[None, "--logdir", str(logs_location)])
+    url = tb.launch()
+    print(f"Tensorflow listening on {url}")
 
-    os.system("tensorboard --logdir=runs")
-    return
 
-
-def relu(x):
-    return max(0.0, x)
+def print_env_info(env: UnityEnvironment) -> None:
+    # get the action space and observation space
+    print(env.behavior_specs)  # noqa: T201
+    behavior_name = next(iter(env.behavior_specs))
+    print(f"Name of the behavior : {behavior_name}")  # noqa: T201
+    spec = env.behavior_specs[behavior_name]
+    observation_shape = spec.observation_specs
+    print(observation_shape)  # noqa: T201
+    num_actions = spec.action_spec
+    print(num_actions)  # noqa: T201
 
 
 if __name__ == "__main__":
-    # set up the environment
-    env_location = ENV_PATH
-    env = UnityEnvironment(
-        file_name=env_location, num_areas=NUM_AREAS, side_channels=[engine_channel, data_channel]
-    )
+    # Start TensorBoard
+    log_location = Path(__file__).parent / "runs"
+    writer = SummaryWriter(log_location / datetime.datetime.now().strftime("%y-%m-%d %H%M%S"))
+    launch_tensor_board(log_location)
 
+    # Start keyboard listener for saving
+    listener = KeyboardListener()
+    listener.start()
+
+    # Set up the environment
+    engine_channel = EngineConfigurationChannel()
+    data_channel = DataChannel()
+    env_location = ENV_PATH
+    if INTERACTIVE:
+        env_location = None
+        print("Waiting for unity environment")  # noqa: T201
+    env = UnityEnvironment(
+        file_name=env_location,
+        num_areas=NUM_AREAS,
+        side_channels=[engine_channel, data_channel],
+    )
     # Wide - 15
     # Slim - 10
     data_channel.set_int_parameter("roadSize", 15)
     # 0 -> Amazon road
     # 1 -> Black & white road
     data_channel.set_int_parameter("roadColor", 0)
-    data_channel.set_int_parameter("cameraWidth", 64)
-    data_channel.set_int_parameter("cameraHeight", 64)
-
     engine_channel.set_configuration_parameters(time_scale=TIME_SCALE)
     env.reset()
 
-    t = threading.Thread(target=launch_tensor_board, args=([]))
-    t.start()
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"-------- Running on {device}")
-
-    # get the action space and observation space
-    print(env.behavior_specs)
-    behavior_name = list(env.behavior_specs)[0]
-    print(f"Name of the behavior : {behavior_name}")
-    spec = env.behavior_specs[behavior_name]
-    observation_shape = spec.observation_specs
-    print(observation_shape)
-    num_actions = spec.action_spec
-    print(num_actions)
+    print(f"Running on {device}")  # noqa: T201
+    print_env_info(env)
 
     num_epochs = MAX_TRAINED_EPOCHS
     temperature = START_TEMPERATURE
     temperature_red = REDUCE_TEMPERATURE
 
-    folder_name = f'{MODEL_PATH}/{datetime.datetime.now().strftime("%y-%m-%d %H%M%S")}'
+    model_folder = Path(MODEL_PATH) / datetime.datetime.now().strftime("%y-%m-%d %H%M%S")
 
     results = []
     try:
@@ -119,33 +122,33 @@ if __name__ == "__main__":
         )
 
         if SAVE_MODEL:
-            print(f"---- Will save all models to {folder_name} ----")
+            print(f"---- Will save all models to {model_folder} ----")  # noqa: T201
         else:
-            print('---- Not saving model as the -s flag is default to "False" ----')
+            print('---- Not saving model as the -s flag is default to "False" ----')  # noqa: T201
 
         for epoch in range(num_epochs):
-            print("------Training------")
-            print(f"Epoch: {epoch}, Temperature:{temperature}")
+            print("------Training------")  # noqa: T201
+            print(f"Epoch: {epoch}, Temperature:{temperature}")  # noqa: T201
             reward = trainer.train(env, temperature)
 
-            print("------Done------")
+            print("------Done------")  # noqa: T201
             reward /= NUM_TRAINING_EXAMPLES
-            print(f"Reward earned: {reward}")
+            print(f"Reward earned: {reward}")  # noqa: T201
 
-            temperature = relu(temperature - temperature_red)
+            temperature = max(0.0, temperature - temperature_red)
             writer.add_scalar("Reward/Train", reward, epoch)
             writer.flush()
 
             if SAVE_MODEL or listener.was_pressed():
-                if not os.path.isdir(folder_name):
-                    os.makedirs(folder_name)
+                folder = Path(model_folder)
+                folder.mkdir(parents=True, exist_ok=True)
 
-                torch.save(qnet, f"{folder_name}/model-epoch-{epoch}.pkl")
-                trainer.save_model(f"{folder_name}/model-epoch-{epoch}.onnx")
+                torch.save(qnet, model_folder / f"model-epoch-{epoch}.pkl")
+                trainer.save_model(model_folder / f"model-epoch-{epoch}.onnx")
                 listener.reset()
 
     except KeyboardInterrupt:
-        print("\nTraining interrupted, continue to next cell to save to save the model.")
+        print("\nTraining interrupted, continue to next cell to save to save the model.")  # noqa: T201
 
     finally:
         env.close()
@@ -154,8 +157,8 @@ if __name__ == "__main__":
     try:
         if NO_DISPLAY:
             training_data = {"num_epochs": len(results), "results": results}
-            with open(f"{folder_name}/training-data.json", "w") as f:
+            with (model_folder / "training-data.json").open("w") as f:
                 json.dump(training_data, f)
-            print(f"Saved training data in {folder_name}/training-data.json")
+            print(f"Saved training data in {model_folder}/training-data.json")  # noqa: T201
     except ValueError:
-        print("\nPlot failed on interrupted training.")
+        print("\nPlot failed on interrupted training.")  # noqa: T201

python_trainer/trainer.py
@@ -140,18 +140,12 @@ class Trainer:
         loss_sum = 0
         count = 0
 
-        for epoch in range(epochs):
+        for _ in range(epochs):
             for batch in dataloader:
                 # We run the training step with the recorded inputs and new Q value targets.
-                X, y = batch
-                # X = [X[0].view((-1, 1, 64, 64)), X[1].view((-1, 1))]
-                # y = y.view(-1, self.model.output_shape[1])
+                x, y = batch
 
-                vis_X = X[0].view((-1, 1, 64, 64))
-                nonvis_X = X[1].view((-1, 1))
-                X = (vis_X, nonvis_X)
-
-                y_hat = self.model(X)
+                y_hat = self.model(x)
                 loss = self.loss_fn(y_hat, y)
                 print("loss", loss)
                 # Backprop
@@ -160,6 +154,7 @@ class Trainer:
                 self.optim.step()
                 loss_sum += loss.item()
                 count += 1
+
         self.writer.add_scalar("Loss/Epoch", loss_sum / count, self.curr_epoch)
         self.curr_epoch += 1
 
@@ -169,7 +164,8 @@ class Trainer:
             (
                 # Vis observation
                 torch.randn((1,) + self.model.visual_input_shape),
-                torch.randn((1,) + self.model.nonvis_input_shape),  # Non vis observation
+                # Non vis observation
+                torch.randn((1,) + self.model.nonvis_input_shape),
             ),
             path,
             opset_version=9,