767c31b29b2e287beae216a67f002ea50380fc4f
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-08-18 23:07:33 +0200

    Experimenting with pretraining

--- src/depth_net/train.py
+++ src/depth_net/train.py
@@ -6,6 +6,32 @@ from tqdm import tqdm
 from dataset import MaskDataset
 
 
+def pretrain(net, dataset, epochs=1) -> None:
+    batch_size = 64
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    y = torch.tensor([[0.0, 10.0, 0.0]] * batch_size, device=net.device)
+    loss_fn = torch.nn.MSELoss()
+
+    for epoch in range(epochs):
+        epoch_loss = 0.0  # Reset each epoch
+        total_samples = 0
+        print(f"Epoch {epoch}")
+        for batch in tqdm(dataloader):
+            x, _ = batch
+            y_hat = net.forward(x)
+            loss = loss_fn(y_hat, y)
+
+            net.optim.zero_grad()
+            loss.backward()
+            net.optim.step()
+
+            epoch_loss += loss.item() * batch_size
+            total_samples += batch_size
+
+        avg_epoch_loss = epoch_loss / total_samples
+        print(f"Average loss: {avg_epoch_loss}")
+
+
 def fit(net, dataset, epochs=1) -> None:
     dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
 
@@ -37,6 +63,7 @@ def main():
     dataset = MaskDataset("dataset/images", "dataset/masks", device, image_size)
     net = DepthNetwork(image_size, device)
     net.to(device)
+    pretrain(net, dataset, epochs=20)
     fit(net, dataset, epochs=100)
 
 
