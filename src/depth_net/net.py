Fixing activation function, going back with rotation representation, lowering threshold for scheduler

src/depth_net/net.py
@@ -39,7 +39,7 @@ class DepthNetwork(torch.nn.Module):
             nn.ReLU(),
             nn.Linear(256, 64),
             nn.ReLU(),
-            nn.Linear(64, 4),
+            nn.Linear(64, 3),
         )
 
         self.optim = torch.optim.Adam(
@@ -49,14 +49,16 @@ class DepthNetwork(torch.nn.Module):
         )
 
         self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            self.optim, mode="min", factor=0.1, patience=3, cooldown=1, threshold=0.01
+            self.optim, mode="min", factor=0.1, patience=3, cooldown=1, threshold=0.001
         )
 
         self.device = device
         self.input_shape = (3, *image_size)
 
         self.projector = Projector("src/depth_net/utils/Prometheus.obj", image_size, self.device)
-        self.focal_length = self.projector.camera.focal_length[0][1].item()
+        self.focal_length = self.projector.camera.focal_length[0][
+            1
+        ].item()  # focal length in image coordinates
         self.image_width = self.projector.image_size[0]
         self.cx = self.image_width / 2  # Principal point (image center)
 
@@ -72,15 +74,16 @@ class DepthNetwork(torch.nn.Module):
     def activation_fn(self, preds):
         x_raw = preds[:, 0]
         y_raw = preds[:, 1]
-        sin_raw = preds[:, 2]
-        cos_raw = preds[:, 3]
-        theta = torch.atan2(torch.tanh(sin_raw), torch.tanh(cos_raw))
+        theta_raw = preds[:, 2]
 
         x_ratio = torch.tanh(x_raw)  # [-1, 1]
         y_dist = F.softplus(y_raw)  # ensure distance is positive
+        theta = torch.tanh(theta_raw) * 180
 
-        # Calculate x coordinate using the physical constraint
-        max_x = y_dist * ((self.image_width) / (2 * self.focal_length))
+        max_x = (y_dist - self.projector.CAMERA_POS[2]) * (
+            (self.projector.SENSOR_WIDTH_MM) / (2 * self.projector.FOCAL_LENGTH_MM)
+        )
+
         x_coord = x_ratio * max_x
 
         return torch.stack([x_coord, y_dist, theta], dim=1)
@@ -94,3 +97,17 @@ class DepthNetwork(torch.nn.Module):
         preds = self.activation_fn(preds)
 
         return preds
+
+
+if __name__ == "__main__":
+    net = DepthNetwork((512, 512), torch.device("cpu"))
+    position_raw = torch.tensor(
+        [[10000, 30.0, 0, 1]],
+        dtype=torch.float32,
+    )
+    position = net.activation_fn(position_raw)
+    print(position)
+    x = position[0][0].item()
+    y = position[0][1].item()
+    theta = position[0][2].item()
+    net.projector.render_mask(x, y, theta)