671b0e882203e61af9f26de446afb58aff890843
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-08-25 00:00:26 +0200

    pretraining 3 epochs

--- src/depth_net/train.py
+++ src/depth_net/train.py
@@ -201,7 +201,7 @@ def main():
     net.to(device)
 
     print("Pretraining...")
-    pretrain(net, train_dataset, writer, epochs=6)
+    pretrain(net, train_dataset, writer, epochs=3)
     print("Fitting...")
     best_net = fit(net, train_dataset, val_dataset, writer, epochs=500)
 
