aec18dda8b3989a3fe4ca85c1f7ff3c7d001ae8d
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-08-26 00:06:25 +0200

    Variable theta when pretraining

--- src/depth_net/train.py
+++ src/depth_net/train.py
@@ -32,6 +32,7 @@ def pretrain(net, dataset, writer, epochs=1) -> None:
             x = x.to(net.device)
             batch_size = x.shape[0]
             y = torch.tensor([[0.0, 10.0, 0.0]] * batch_size, device=net.device)
+            y[:, 2] = torch.rand(batch_size, device=net.device) * 360 - 180
             y_hat = net.forward(x)
             loss = loss_fn(y_hat, y)
 
