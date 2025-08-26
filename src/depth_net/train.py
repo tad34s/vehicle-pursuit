6f559ad7112ad37e4517573a0685106c1df860da
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-08-26 17:49:07 +0200

    Experimenting with active learning

--- src/depth_net/train.py
+++ src/depth_net/train.py
@@ -344,7 +344,7 @@ def main():
     net.to(device)
 
     print("Pretraining...")
-    pretrain(net, train_dataset, writer, epochs=2)
+    pretrain(net, train_dataset, writer, epochs=3)
     print("Fitting...")
     best_net = fit(net, train_dataset, val_dataset, writer, epochs=500)
     test_dataset = TestDataset(
