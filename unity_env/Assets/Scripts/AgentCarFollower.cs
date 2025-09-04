5fd3ed4b5866b8ce786a061530bc17dda64955f7
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-09-04 22:06:58 +0200

    Adding cont action space for follower

--- unity_env/Assets/Scripts/AgentCarFollower.cs
+++ unity_env/Assets/Scripts/AgentCarFollower.cs
@@ -15,12 +15,10 @@ public class AgentCarFollower : Agent
 
     private bool pauseLearning = false;
 
-    const int k_Forward = 0;
-    const int k_Back = 1;
-    const int k_Left = 2;
-    const int k_Right = 3;
+    const int k_Speed = 0;
+    const int k_Steering = 1;
 
-    public PrometeoCarController carController;
+    public PrometeoCarControllerCont carController;
 
     public Camera carCamera;
 
@@ -119,18 +117,14 @@ public class AgentCarFollower : Agent
     }
 
 
-    void TriggerAction(ActionBuffers actions)
+   void TriggerAction(ActionBuffers actions)
     {
-        bool goForward = actions.DiscreteActions[k_Forward] == 1;
-        // bool goForward = true;
-        bool goBack = actions.DiscreteActions[k_Back] == 1;
-        // bool goBack = false;
-        bool turnLeft = actions.DiscreteActions[k_Left] == 1;
-        bool turnRight = actions.DiscreteActions[k_Right] == 1;
+        float speed = actions.ContinuousActions[k_Speed];
+        float steering = actions.ContinuousActions[k_Steering];
 
-        carController.Movement(true, goForward, goBack, turnLeft, turnRight);
+        carController.Movement(true, speed, steering);
     }
-
+        
     public override void OnActionReceived(ActionBuffers actions)
     {
         if (pauseLearning)
@@ -154,11 +148,20 @@ public class AgentCarFollower : Agent
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        var discreteActionsOut = actionsOut.DiscreteActions;
+        var continuousActionsOut = actionsOut.ContinuousActions;
 
-        discreteActionsOut[k_Forward] = Input.GetKey(KeyCode.W) ? 1 : 0;
-        discreteActionsOut[k_Back] = Input.GetKey(KeyCode.S) ? 1 : 0;
-        discreteActionsOut[k_Left] = Input.GetKey(KeyCode.A) ? 1 : 0;
-        discreteActionsOut[k_Right] = Input.GetKey(KeyCode.D) ? 1 : 0;
+        continuousActionsOut[k_Speed] = 0;
+        if (Input.GetKey(KeyCode.W))
+            continuousActionsOut[k_Speed] += 1;
+
+        if (Input.GetKey(KeyCode.S))
+            continuousActionsOut[k_Speed] -= 1;
+
+        continuousActionsOut[k_Steering] = 0;
+        if (Input.GetKey(KeyCode.D))
+            continuousActionsOut[k_Steering] += 1;
+
+        if (Input.GetKey(KeyCode.A))
+            continuousActionsOut[k_Steering] -= 1;
     }
 }
